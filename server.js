50de143b4a76823b5c9add5cdb563cba32788cbb
John Murphy <chiefsmurph@gmail.com>
2016-09-03 23:15:43 -0700

    moving ahead

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var pg = require('pg');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
@@ -8,3 +9,13 @@ server.listen(port);
 console.log('listening on ' + port);
 
 app.use(express.static(__dirname + '/public'));
+
+pg.connect(process.env.DATABASE_URL + "?ssl=true", function(err, client, done) {
+  var queryText = 'CREATE TABLE users (id serial primary key, url, timestamp timestamp default current_timestamp )';
+  client.query(queryText, function(err, result) {
+
+    done();
+    users = result.rows;
+
+  });
+});
