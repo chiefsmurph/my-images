5cdc067cd7d9bc7d0f13392730bc405692515f4d
John Murphy <chiefsmurph@gmail.com>
2016-09-04 00:30:00 -0700

    now with database

---
+++ dbRelated.js
@@ -0,0 +1,29 @@
+var pg = require('pg');
+
+module.exports = {
+  importantQuerys: {
+    deleteAll: 'DELETE FROM sunshine',
+    createTable: 'CREATE TABLE sunshine (id serial primary key, url VARCHAR(200) not null, timestamp timestamp default current_timestamp )',
+    getAll: 'SELECT * FROM sunshine',
+    insert: 'INSERT INTO sunshine (url) VALUES($1) RETURNING *'
+  },
+  query: function(queryText, passVals, cb) {
+    if (!cb) { cb = passVals; }
+    pg.connect(process.env.DATABASE_URL + "?ssl=true", function(err, client, done) {
+      if (err) {
+        return cb(err);
+      }
+      client.query(queryText, passVals || [], function(err, result) {
+        cb(err, result);
+        done();
+      });
+    });
+  },
+  run: function(queryName, cb) {
+    this.query(this.importantQuerys[queryName], cb);
+  },
+  addPost: function(url, cb) {
+    console.log('adding' + url);
+    this.query(this.importantQuerys.insert, [url], cb);
+  }
+}
--- server.js
+++ server.js
@@ -1,21 +1,77 @@
-var pg = require('pg');
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+var dbRelated = require('./dbRelated');
+
 var port = process.env.PORT || 5000;
+
+
 server.listen(port);
 
 console.log('listening on ' + port);
 
 app.use(express.static(__dirname + '/public'));
 
-pg.connect(process.env.DATABASE_URL + "?ssl=true", function(err, client, done) {
-  var queryText = 'CREATE TABLE users (id serial primary key, url, timestamp timestamp default current_timestamp )';
-  client.query(queryText, function(err, result) {
 
-    done();
-    users = result.rows;
+// START
 
+var allPosts = [];
+function getAllPosts() {
+  dbRelated.run('getAll', function(err, result) {
+    if (!err) {
+      console.log('refreshed database');
+      allPosts = result.rows;
+    } else {
+      console.log(err);
+    }
   });
+}
+
+(function createSunshineTable() {
+  // dbRelated.query('ALTER TABLE sunshine ALTER COLUMN url TYPE varchar(200);', function(err, result) {
+  //   console.log(err, result)
+  // });
+  dbRelated.run('createTable', function(err, result) {
+    if (err && err.code === '42P07') {
+      console.log('table already exists');
+
+      //dbRelated.run('deleteAll', function(err, result) {
+        //console.log('deletedall');
+        getAllPosts();
+      //});
+    } else if (err) {
+      console.log('error creating db...');
+      console.log(err);
+    } else {
+      console.log('sunshine table created');
+    }
+  });
+})();
+
+
+
+
+app.post('/post', function(req, res, next) {
+  console.log(req.body);
+  var url = req.body.url;
+  if (url) {
+    dbRelated.addPost(url, function(err, result) {
+      res.json({
+        err: err,
+        result: result
+      });
+      console.log(err, result);
+      allPosts.push(result.rows[0]);
+    });
+  }
+});
+
+app.get('/posts', function(req, res, next) {
+  res.json(allPosts);
 });
